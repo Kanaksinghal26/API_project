a852f7526c5531712aab3cdb6117abcf01462274
Kanak Singhal <kanaksinghal98@gmail.com>
2022-05-19 15:08:38 +0530

    initial commit

---
+++ src/doc_Apps/Doc1_1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Tut1_1 from '../tutorial/Tut1_1'
+
+const Doc1_1 = () => {
+  return (
+    <Tut1_1 />
+  )
+}
+
+export default Doc1_1;
---
+++ src/tutorial/Tut1_1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Tut1_1.css'
+
+const Tut1_1 = () => {
+  return (
+    <div>The Error has been solved through Appyarts</div>
+  )
+}
+
+export default Tut1_1;
