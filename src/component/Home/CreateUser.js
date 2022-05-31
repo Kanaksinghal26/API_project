5c642949f2a299952f1106ea4e263196a0428241
Kanak Singhal <kanaksinghal98@gmail.com>
2022-05-31 07:36:34 +0530

    Updated CreateUser page

--- src/component/Home/CreateUser.js
+++ src/component/Home/CreateUser.js
@@ -1,22 +1,92 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 const CreateUser = () => {
+
+	const[username, setUsername] = React.useState()
+	const[email, setEmail] = React.useState()
+	const[fullname, setFullname] = React.useState()
+	const[phone, setPhone] = React.useState()
+	const[website, setWebsite] = React.useState()
+	const[street, setStreet] = React.useState()
+	const[suite, setSuite] = React.useState()
+	const[city, setCity] = React.useState()
+	const[zipcode, setZipcode] = React.useState()
+	const[company, setCompany] = React.useState()
+
+	const handleusername = (e) => {
+		setUsername(e.target.vale)
+	}
+
+	const handleemail = (e) => {
+		setEmail(e.target.vale)
+	}
+
+	const handlefullname = (e) => {
+		setFullname(e.target.vale)
+	}
+
+	const handlephone = (e) => {
+		setPhone(e.target.vale)
+	}
+
+	const handlewebsite = (e) => {
+		setWebsite(e.target.vale)
+	}
+
+	const handlestreet = (e) => {
+		setStreet(e.target.vale)
+	}
+
+	const handlesuite = (e) => {
+		setSuite(e.target.vale)
+	}
+
+	const handlecity = (e) => {
+		setCity(e.target.vale)
+	}
+
+	const handlezipcode = (e) => {
+		setZipcode(e.target.vale)
+	}
+
+	const handlecompany = (e) => {
+		setCompany(e.target.vale)
+	}
+
     return (
         <>
-        <div>
-            New Registration List
-        </div>
+			<div>
+				<h1>New Registration List</h1>
+			</div>
 
-        <Box
-          sx={{
-              width: 500,
-              maxWidth: '100%',
-            }}
-            >
-          <TextField fullWidth label="fullWidth" id="fullWidth" />
-        </Box>
+			<Box className='addnew'
+			sx={{
+				width: 500,
+				maxWidth: '100%',
+				}}
+				>
+
+			<TextField fullWidth label="UserName" id="fullWidth" onClick={handleusername} />
+			<TextField fullWidth label="User Email" id="fullWidth" onClick={handleemail} />
+			<TextField fullWidth label="Ful Name" id="fullWidth" onClick={handlefullname} />
+			<TextField fullWidth label="Phone" id="fullWidth" onClick={handlephone} />
+			<TextField fullWidth label="Website URL" id="fullWidth" onClick={handlewebsite} />
+			<TextField fullWidth label="Street" id="fullWidth" onClick={handlestreet} />
+			<TextField fullWidth label="Enter Suite" id="fullWidth" onClick={handlesuite} />
+			<TextField fullWidth label="Enter City" id="fullWidth" onClick={handlecity} />
+			<TextField fullWidth label="Enter ZipCode" id="fullWidth" onClick={handlezipcode} />
+			<TextField fullWidth label="Company Name" id="fullWidth" onClick={handlecompany} />
+
+			</Box>
         </>
       );
 }
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './Home.scss'
-import UserCard from './UserCard';
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
 	Link
-  } from "react-router-dom";
-  import { Button, ButtonGroup } from '@chakra-ui/react'
+} from "react-router-dom";
+import './Home.scss'
+import UserCard from './UserCard';
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 const Home = () => {
 
@@ -21,59 +21,59 @@ const Home = () => {
 
 	const fetchData = () => {
 		axios.get("https://jsonplaceholder.typicode.com/users")
-		.then((res) =>{
-			setUserList(res.data)
-			console.log(res)
-		})
-		.catch(err =>{
-			console.log("error")
-		})
+			.then((res) => {
+				setUserList(res.data)
+				console.log(res)
+			})
+			.catch(err => {
+				console.log("error")
+			})
 	}
 
 
 
-	const handleSearch = (e) =>{
+	const handleSearch = (e) => {
 		setSearchText(e.target.value)
 	}
-	
-	const handleSearchEmail = (e) =>{
+
+	const handleSearchEmail = (e) => {
 		setSearchTextEmail(e.target.value)
 	}
-	
-	const handleSearchPhone = (e) =>{
+
+	const handleSearchPhone = (e) => {
 		setSearchTextPhone(e.target.value)
 	}
-	
-	const handleSearchFullName = (e) =>{
+
+	const handleSearchFullName = (e) => {
 		setSearchTextFullName(e.target.value)
 	}
-	
+
 	useEffect(() => {
 		fetchData()
 	}, [])
 
-	const filterList =() => {
-		if(searchText){
+	const filterList = () => {
+		if (searchText) {
 			return userList && userList.filter(ul => ul.username
 				.toLowerCase().startsWith(searchText.toLowerCase()))
 		}
-		
-		else if(searchTextEmail){
+
+		else if (searchTextEmail) {
 			return userList && userList.filter(ul => ul.email.toLowerCase().startsWith(searchTextEmail.toLowerCase()))
 		}
 
-		else if(searchTextPhone){
+		else if (searchTextPhone) {
 			return userList && userList.filter(ul => ul.phone.toLowerCase().startsWith(searchTextPhone.toLowerCase()))
 		}
 
-		else if(searchTextFullName){
+		else if (searchTextFullName) {
 			return userList && userList.filter(ul => ul.name.toLowerCase().startsWith(searchTextFullName.toLowerCase()))
 		}
 
-		else{
+		else {
 			return userList
-			
-		}	
+
+		}
 	}
 
 	// const filterListEmail =() => {
@@ -85,37 +85,32 @@ const Home = () => {
 	return (
 		<div className='home'>
 			<div className='home-container flex flex-col'>
-			<div className='home-container-heading'><h1>List of users</h1></div>
-			<div className='home-container-search'>
-				<input onChange={handleSearchFullName} className="input-search" type="text" placeholder="Search by Full name" id='search4' />
-				<input onChange={handleSearch} className="input-search" type="search" placeholder="Search by username" id='search1' />
-				<input onChange={handleSearchEmail} className="input-search" type="search" placeholder="Search by email" id='search2' />
-				<input onChange={handleSearchPhone} className="input-search" type="number" placeholder="Search by phone" id='search3' />
-			</div>
-			<div className='home-container-list flex flex-wrap' style={{gap: '2rem'}}>
-				{
-				filterList() && filterList().map(ab => {
-					return <UserCard item={ab} />
-				})
+				<div className='home-container-heading'><h1>List of users</h1></div>
 
-				// filterList() && filterList().map(ab => {
-				// 	return <UserCard item={ab} />
-				// })
+				<div className='home-container-search'>
+					<input onChange={handleSearchFullName} className="input-search" type="text" placeholder="Search by Full name" id='search4' />
+					<input onChange={handleSearch} className="input-search" type="search" placeholder="Search by username" id='search1' />
+					<input onChange={handleSearchEmail} className="input-search" type="search" placeholder="Search by email" id='search2' />
+					<input onChange={handleSearchPhone} className="input-search" type="number" placeholder="Search by phone" id='search3' />
+				</div>
 
+				<div className='home-container-list flex flex-wrap' style={{ gap: '2rem' }}>
+					{
+						filterList() && filterList().map(ab => {
+							return <UserCard item={ab} />
+						})
+					}
+				</div>
 
-				}
-			</div>
-			<div id='linkPart'>
-			<Link to={'/createuser'}
-                                    >
-
-			<Button colorScheme='blue'>Button</Button>
-                                 </Link>
-			</div>
+				<div id='linkPart'>
+					<Link to={'/createuser'}>
+						<Button colorScheme='blue'>+ CreateUser</Button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	)
-	}
+}
 
 export default Home
 
@@ -127,3 +122,7 @@ export default Home
 // 4. Weather Api.
 // 5. City details.
 
+
+// filterList() && filterList().map(ab => {
+// 	return <UserCard item={ab} />
+// })
